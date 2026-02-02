d2ac8eb9f23dc28d088b1e563f5c65cc0704a061
Lautaro Guzzio <lauguzzio@gmail.com>
2026-02-01 22:34:43 -0300

    Initial commit

---
+++ src/main/java/io/github/guzziolautaro/dsBotIntegration/DsBotIntegration.java
@@ -0,0 +1,21 @@
+package io.github.guzziolautaro.dsBotIntegration;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public final class DsBotIntegration extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    private void sendHeartbeat() {
+        //todo
+    }
+}
